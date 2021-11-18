5a70f373737b5b62d4a1b659d1938709d750f0f4
LuSrodri <lucassrodri08@gmail.com>
2021-11-17 21:56:27 -0300

    Vuetify - criando app Vuetify

--- src/App.vue
+++ src/App.vue
@@ -99,6 +99,26 @@
           {{ new Date().getFullYear() }} — <strong>Lucas Santos</strong>
         </v-col>
       </v-footer>
+
+      <v-bottom-navigation app :value="value" color="primary">
+        <v-btn>
+          <span>Recents</span>
+
+          <v-icon>mdi-history</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Favorites</span>
+
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Nearby</span>
+
+          <v-icon>mdi-map-marker</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
     </div>
   </v-app>
 </template>
